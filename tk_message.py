465bc29004fcc7bd98c409406d128a3b2fa249e1
Damien <damomurtagh@gmail.com>
2019-04-11 22:26:56 +1000

    message example

--- tk_message.py
+++ tk_message.py
@@ -3,11 +3,11 @@ from tkinter import *
 # **** Create window ****
 root = Tk()
 root.geometry("300x200")
-root.title("Tkinter Create Window")
+root.title("Tkinter Message Example")
 
 # **** Add window content ****
-example_label = Label(root, text="This is the example label", bg="light blue")
-example_label.pack()
+example_message = Message(root, text="Mesages are just like lables, except their text can go over several lines.", bg="light blue", width=150)
+example_message.pack()
 
 # **** Run window loop ****
 root.mainloop()
