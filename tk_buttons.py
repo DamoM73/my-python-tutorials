0592c9b0c4af653df5dde4d8367010baaf072c0b
Damien <damomurtagh@gmail.com>
2019-04-11 22:04:47 +1000

    button example

---
+++ tk_buttons.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+# **** Functions ****
+def change_colour():
+    global colour
+    
+    colour = (colour + 1) % len(colours)
+    change_colour_btn.config(bg=colours[colour])
+
+# **** Global Variables ****
+colour = 0
+colours = ['blue','red','yellow','green','purple', 'orange', 'white']
+
+
+# **** Create window ****
+root = Tk()
+root.geometry("300x200")
+root.title("Tkinter Button Example")
+
+# **** Add content to window ****
+change_colour_btn = Button(root, text="Click to change colour", bg=colours[colour],  command=change_colour)
+change_colour_btn.pack()
+
+# **** Run window loop ****
+root.mainloop()
