8a9377b695aaab9013c702d178777d94dfc63ae9
DamoM73 <damomurtagh@gmail.com>
2019-06-05 18:38:25 +1000

    scratch stuff

--- scratch.py
+++ scratch.py
@@ -1,13 +1,5 @@
 from tkinter import *
 
-master = Tk()
-variable = StringVar()
-w = OptionMenu(master, variable, "one", "two", "three")  
-w.config(bg = "GREEN")  # Set background color to green
-
-# Set this to what you want, I'm assuming "green"...
-w["menu"].config(bg="GREEN")
-
-w.pack() 
-
-master.mainloop()
+# **** create window ****
+root = Tk()
+root.geometry
